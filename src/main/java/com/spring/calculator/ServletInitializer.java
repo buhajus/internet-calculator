fd64465b74d86c38aa9d5a780da7d28d09ec5fbe
Buhajus <domassteimantas@gmail.com>
2023-04-19 21:41:59 +0300

    v.0.2

--- src/main/java/com/spring/calculator/ServletInitializer.java
+++ src/main/java/com/spring/calculator/ServletInitializer.java
@@ -3,6 +3,9 @@ package com.spring.calculator;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+//Servlet yra JAVA programa veikianti web serberyje
+//Servlet paleidžiamas kai varotojojas paspaudžia nuorodą, pateikia formą ar atlieka kito tipo veiksmus webe
+// Kiekviena kliento užklasusa (request) praeina per servlet, kuris jį perduoda Controlerio request mapping atributui
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
